e8f7acce6278eaf23c02a9af18554347b59f2a4e
Artur Barseghyan <barseghyan@goldmund-wyldebeast-wunderliebe.com>
2014-06-12 09:18:23 +0200

    Initial work on tracking user IPs and helper views to view the tracked data.

--- src/collective/smsauthenticator/browser/list_user_ips.py
+++ src/collective/smsauthenticator/browser/list_user_ips.py
@@ -1,3 +1,5 @@
+import datetime
+
 from zope.i18nmessageid import MessageFactory
 
 from plone import api
@@ -16,6 +18,10 @@ class ListUserIPs(BrowserView):
 
     def _get_user_unique_ips(self, user):
         """
+        Gets unique IPs of a user.
+
+        :param Products.PlonePAS.tools.memberdata user:
+        :return: Set of user unique IPs.
         """
         unique_ips = set()
         raw_ips = user.getProperty('ips')
@@ -24,8 +30,39 @@ class ListUserIPs(BrowserView):
             all_ips = raw_ips.split('\n')
 
             for ip_line in all_ips:
-                ip, visit = ip_line.split(',', 2)
-                unique_ips.append(ip)
+                if ip_line:
+                    try:
+                        ip, visit = ip_line.split(',', 2)
+                        unique_ips.add(ip)
+                    except Exception as e:
+                        pass
+
+        return unique_ips
+
+    def _get_user_ips(self, user):
+        """
+        Gets all IPs of a user.
+
+        :param Products.PlonePAS.tools.memberdata user:
+        :return: List of user IPs.
+        """
+        unique_ips = []
+        raw_ips = user.getProperty('ips')
+
+        if raw_ips:
+            all_ips = raw_ips.split('\n')
+
+            for ip_line in all_ips:
+                if ip_line:
+                    try:
+                        ip, visit = ip_line.split(',', 2)
+                        try:
+                            visit = datetime.datetime.fromtimestamp(float(visit))
+                        except Exception as e:
+                            pass
+                        unique_ips.append((ip, visit))
+                    except Exception as e:
+                        pass
 
         return unique_ips
 
@@ -43,5 +80,8 @@ class ListUserIPs(BrowserView):
         """
         List all user IPs.
         """
-        return 'all'
-
+        users = api.user.get_users()
+        ips = []
+        for user in users:
+            ips += self._get_user_ips(user)
+        return ips
